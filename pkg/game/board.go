36549578091e9085b1701f46fdb007648bae54eb
tekkac <tarik@carbonable.io>
2024-04-08 02:11:33 +0200

    🏗️ add secret and pubkey generation for commitment

--- pkg/game/board.go
+++ pkg/game/board.go
@@ -3,7 +3,12 @@ package game
 import (
 	"math/rand"
 
+	"github.com/NethermindEth/juno/core/felt"
+
 	"github.com/MartianGreed/memo-backend/pkg/data"
+	"github.com/MartianGreed/memo-backend/pkg/starknet"
+	"github.com/NethermindEth/juno/core/crypto"
+	"fmt"
 )
 
 type Tile struct {
@@ -12,28 +17,63 @@ type Tile struct {
 }
 type Board struct {
 	grid     [][]data.Attributes `json:"grid"`
+	secrets   []FeltPair
+	pubkeys   []*felt.Felt
 	Revealed [][]Tile            `json:"revealed"`
+	g1 felt.Felt
+	g2 felt.Felt
+}
+
+type FeltPair struct {
+	key felt.Felt
+	other bool
+}
+
+type DistinguishedPair struct {
+	data data.Attributes
+	other bool
 }
 
 func CreateBoard(collection *data.Collection) *Board {
+	server_seed := starknet.FeltFromInt(rand.Intn(9999999))
+	g1 := starknet.FeltFromInt(rand.Intn(9999999) + 1)
+	g2 := starknet.FeltFromInt(rand.Intn(9999999) + 1)
+
 	pairs := collection.GetPairs()
-	pairs = append(pairs, pairs...)
+	var originals []DistinguishedPair
+	var copies []DistinguishedPair
+	var tiles []DistinguishedPair
+
+    originals = Map(pairs, func(p data.Attributes) DistinguishedPair {  return DistinguishedPair {data: p, other: false }})
+	copies = Map(pairs, func(p data.Attributes) DistinguishedPair { return DistinguishedPair {data: p, other: true }})
+    tiles = append(originals, copies...)
+
+	fmt.Println(len(tiles))
 
 	rand.Shuffle(len(pairs), func(i, j int) { pairs[i], pairs[j] = pairs[j], pairs[i] })
 
 	// create 6x10 grid
 	// place randomly 30 pairs of cards
 	var grid [][]data.Attributes
+	var secrets []FeltPair
+
 	count := 0
 	for i := 0; i < 6; i++ {
 		var row []data.Attributes
 		for j := 0; j < 10; j++ {
-			row = append(row, pairs[count])
+			row = append(row, tiles[count].data)
+
+			tokenId := starknet.FeltFromInt(tiles[count].data.TokenId)
+			key := crypto.PoseidonArray(server_seed, tokenId)
+			secrets = append(secrets, FeltPair { key: *key, other: tiles[count].other })
 			count++
 		}
 		grid = append(grid, row)
 	}
+
 	row := make([]Tile, 10)
+
+	pubkeys := GenPublicKeys(secrets, *g1, *g2)
 
 	return &Board{
 		grid: grid,
@@ -45,5 +85,17 @@ func CreateBoard(collection *data.Collection) *Board {
 			row,
 			row,
 		},
+		secrets: secrets,
+		pubkeys: pubkeys,
+		g1: *g1,
+		g2: *g2,
 	}
+}
+
+func Map[T, U any](ts []T, f func(T) U) []U {
+    us := make([]U, len(ts))
+    for i := range ts {
+        us[i] = f(ts[i])
+    }
+    return us
 }
